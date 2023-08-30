add3e024c9f315b34beb2c5f46fb9db62ce07ed6
The Buny Project <112536156+bunyCloud@users.noreply.github.com>
2023-08-30 13:52:51 -0500

    Update UserKeyStorage.sol

--- contracts/UserKeyStorage.sol
+++ contracts/UserKeyStorage.sol
@@ -5,13 +5,13 @@ contract UserKeyStorage {
     
     struct UserKeyInfo {
         address userAddress;
-        string usernname;
+        string username; // Corrected the typo "usernname" to "username"
         string encryptedKey;
     }
     
     UserKeyInfo[] public userKeys;
     mapping(address => string) public userToKey;
-    event UserAdded(address indexed userAddress,string username, string encryptedKey);
+    event UserAdded(address indexed userAddress, string username, string encryptedKey);
     
     function addWallet(address _userAddress, string memory _username, string memory _encryptedKey) public {
         require(bytes(userToKey[_userAddress]).length == 0, "User already exists");
@@ -21,20 +21,33 @@ contract UserKeyStorage {
     }
 
     function getUserInfo(address _userAddress) public view returns (UserKeyInfo memory) {
-    for (uint256 i = 0; i < userKeys.length; i++) {
-        if (userKeys[i].userAddress == _userAddress) {
-            return userKeys[i];
+        for (uint256 i = 0; i < userKeys.length; i++) {
+            if (userKeys[i].userAddress == _userAddress) {
+                return userKeys[i];
+            }
         }
+        revert("User not found");
     }
-    revert("User not found");
-}
 
-    
     function getEncryptedKey(address _userAddress) public view returns (string memory) {
         return userToKey[_userAddress];
     }
     
     function getTotalUsers() public view returns (uint256) {
         return userKeys.length;
+    }
+
+    function getAllUsers() public view returns (address[] memory, string[] memory, string[] memory) {
+        address[] memory addresses = new address[](userKeys.length);
+        string[] memory usernames = new string[](userKeys.length);
+        string[] memory encryptedKeys = new string[](userKeys.length);
+
+        for (uint256 i = 0; i < userKeys.length; i++) {
+            addresses[i] = userKeys[i].userAddress;
+            usernames[i] = userKeys[i].username;
+            encryptedKeys[i] = userKeys[i].encryptedKey;
+        }
+
+        return (addresses, usernames, encryptedKeys);
     }
 }
